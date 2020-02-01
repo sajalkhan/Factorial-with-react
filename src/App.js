47dab930d275f562606aba5c8e202557a7bd9e8a
sajalkhan <sajalhossen@yahoo.com>
2020-02-01 20:03:15 +0600

    implement factorial functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState({
+    number: '' 
+  });
+
+  const [fstate, fsetState] = useState({
+    result: '' 
+  });
+
+
+  const inputValue = (event)=>{
+    setState({[event.target.name]:event.target.value});
+  }
+
+  const calculateFactorial = (event)=>{
+    const number = state.number;
+    
+    let result = 1;
+    for(let i =1; i<=number; i++) result*=i;
+    fsetState({result: result});
+
+    event.preventDefault();
+  }
+
+  const style = {
+    width: "350px"
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Factorial Calculator</h2>
+        <form onSubmit={inputValue}>
+          <input style={style} type="number" className="form-control" placeholder="Enter a number..." name="number" value={state.number} onChange={inputValue}/>
+          <br/><button className="btn btn-info" onClick={calculateFactorial}>Calculate Factorial</button>
+        </form>
+
+        <h4>Factorial: {fstate.result}</h4>
       </header>
     </div>
   );
